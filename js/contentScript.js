82d2334ba3570f60fef01c7e7a5f357d83f5a61d
Tiaan Swart <tiaanswart@gmail.com>
2018-01-28 11:25:21 +1300

    Add new Quality List control, Add Search for Movies already watched, Introduced promises

--- js/contentScript.js
+++ js/contentScript.js
@@ -42,136 +42,155 @@ const removeOnClickEvents = () => {
 }
 
 const sortMovies = (theMovieList) => {
-	// Run through each and sort it
-	theMovieList.find('.ml-item').sort(function(a, b) {
-		// Sort decending
-	    return +b.dataset.sorthandle - +a.dataset.sorthandle;
-	})
-	// Add back to list
-	.appendTo(theMovieList);
-}
-
-const paginate = (theMovieListWrapper, appendingMovieList) => {
-	// Get progress bar
-	let theProgressBar = appendingMovieList.find('.progress-bar');
-	// Get the progress
-	let theProgress = typeof theProgressBar.data('progress') !== 'undefined' ? theProgressBar.data('progress') : 1;
-	// If we have pagination and we havent reached the search limit yet
-	if (theMovieListWrapper.find('.pagination li.active').length && theProgress < pagesToSearch) {
-		// Get the page links
-		theMovieListWrapper.find('.pagination li.active').nextAll().each(function( index ) {
-			// Get the link
-			let theLink = $(this).find('a').attr('href');
-			// Now get the page from the link
-			$.ajax({
-				url: theLink,
-				method: "GET"
-			}).done(function(data) {
-				// We'll increment the progress bar
-				theProgressBar.css('width', (100 / pagesToSearch * theProgress)+'%');
-				// We'll increment the progress
-				theProgressBar.data('progress', theProgress + 1);
-				// We have the next set of movies, so get the info
-				getMoviesInfo($(data).find('.movies-list-wrap'), appendingMovieList);
-			})
-			.fail(function(xhr) {
-				// Log the error
-				console.log('error', xhr);
-			});
-			return false;
-		});
-		// We're done with it so remove it
-		theMovieListWrapper.find('#pagination').remove();
-	} else {
-		// Remove the progress bar. We are done
-		theProgressBar.parent().remove();
-	}
-	// No one likes to go looking for the good ones :)
-	sortMovies(appendingMovieList);
-}
-
-const getMoviesInfo = (theMovieListWrappers, appendingMovieList, tabIndex) => {
-	// For each list on the page with movies
-	theMovieListWrappers.each(function( index ) {
-		// Get the wrapped list of movies
-		let theMovieListWrapper = $(this);
-		// Help handle tabbing here
-		let theChild = typeof tabIndex === 'undefined' || appendingMovieList == null ? 
-							(theMovieListWrapper.find('.movies-list').length > 1 ? ':first-child' : '') : ':nth-child('+tabIndex+')';
-		// Get the full list of movies, show it incase we lost it
-		let theMovieList = theMovieListWrapper.find('.movies-list'+theChild).show();
-		// Set the list that needs to be applied to
-		let theAppendingList = typeof appendingMovieList === 'undefined' || appendingMovieList == null ? theMovieList : appendingMovieList;
-		// Add the progress bar if we dont have one
-		if (!theAppendingList.find('.progress').length) {
-			theAppendingList.prepend(
-				'<div class="progress">'+
-					'<div class="progress-bar progress-bar-striped active" role="progressbar" style="width:0%">'+
-						'<span style="color:black;padding:10px 0;">Loading</span>'+
-					'</div>'+
-				'</div>'
-			);
-		}
-		// A bit creepy but clone and yeah dig in
-		theMovieList.find('.ml-item').each(function( index ) {
-			// Get the elements we need
-			let theMovieElem = $(this);
-			let theMovieMaskElem = theMovieElem.find('.ml-mask');
-			let thHeadingElem = theMovieElem.find('.mli-info');
-			let theQualityElem = theMovieElem.find('.jtip-quality');
-			let theTootipTopElem = theMovieElem.find('.jtip-top');
-			// Get the title
-			let theMovieTitle = theMovieMaskElem.attr('title');
-			// Get the year
-			let theYear = theTootipTopElem.find('.jt-info:nth-child(2)').text();
-			// Get the full movie name
-			let fullMovieName = theMovieTitle + ' (' + theYear + ')';
-			// Check that its HD, has no duplicate and has not been marked as watched
-			if (theQualityElem.text() == 'HD' 
-				&& theAppendingList.find('.ml-item[title="'+theMovieTitle+'"]').length == 0
-				&& !watchedMovies.includes(fullMovieName)) {
-				// Okay we have a good quality movie
-				// Now the fun starts
-				// Get the info we want so badly from the tooltip top
-				let theNewInfoElem = theTootipTopElem.clone()
-													 .removeClass('jtip-top')
-													 .addClass('mli-quality')
-													 .css({'left' : 8, 'right' : 'auto'});
-				// Now add the info right after the heading elem
-				thHeadingElem.after(theNewInfoElem);
-				let theIMDbRating = theTootipTopElem.find('.jt-imdb').text().replace('IMDb: ','');
-				// We need to record the IMDB rating + the year for sorting
-				theMovieElem.attr('data-sorthandle', theYear+theIMDbRating);
-				// Set the title
-				theMovieElem.attr('title', fullMovieName);
-				// Set the mask target and set the link to play mode
-				theMovieMaskElem.attr('target','_blank')
-								.attr('href', theMovieMaskElem.attr('href') + '?play=1');
-				// Add the movie back in
-				theAppendingList.append(theMovieElem);
-				// Make sure the thumb is visible
-				theMovieElem.find('.mli-thumb').attr('style','');
-				// Add button to top for removing
-				let theButton = $('<a>Remove</a>').addClass('jtip-quality markAsWatchedBtn text-center')
-									 			  .css({'top':0,'width':'100%','height':18,'border-radius':'5px 5px 0 0','padding-top':'5px','z-index':9});
-				theMovieElem.prepend(theButton);
-				// Set additional styling
-				theMovieMaskElem.css({'padding-top':'18px'});
-				theMovieMaskElem.find('.mli-quality, .mli-eps').css({'top':'26px'});
-				theButton.hover(function() { 
-				    $(this).css({'cursor':'pointer'});
-				})
-			} else {
-				theMovieElem.remove();
-			}
-		});
-		// Now some more helpfulness, no one likes to go clicking buttons for the next page!
-		paginate(theMovieListWrapper, theAppendingList);
+    return new Promise(function(resolve, reject) {
+        // Run through each and sort it
+        theMovieList.find('.ml-item').sort(function(a, b) {
+            // Sort decending
+            return +b.dataset.sorthandle - +a.dataset.sorthandle;
+        })
+        // Add back to list
+        .appendTo(theMovieList);
+        resolve(true);
 	});
 }
 
+const paginate = (theMovieListWrapper, appendingMovieList) => {
+    return new Promise(function(resolve, reject) {
+        // Get progress bar
+        let theProgressBar = appendingMovieList.find('.progress-bar');
+        // Get the progress
+        let theProgress = typeof theProgressBar.data('progress') !== 'undefined' ? theProgressBar.data('progress') : 1;
+        // If we have pagination and we havent reached the search limit yet
+        if (theMovieListWrapper.find('.pagination li.active').length && theProgress < pagesToSearch) {
+            // Get the page links
+            theMovieListWrapper.find('.pagination li.active').nextAll().each(function( index ) {
+                // Get the link
+                let theLink = $(this).find('a').attr('href');
+                // Now get the page from the link
+                $.ajax({
+                    url: theLink,
+                    method: "GET"
+                }).done(function(data) {
+                    // We'll increment the progress bar
+                    theProgressBar.css('width', (100 / pagesToSearch * theProgress)+'%');
+                    // We'll increment the progress
+                    theProgressBar.data('progress', theProgress + 1);
+                    // We have the next set of movies, so get the info
+                    getMoviesInfo($(data).find('.movies-list-wrap'), appendingMovieList);
+                })
+                .fail(function(xhr) {
+                    // Log the error
+                    console.log('error', xhr);
+                });
+                return false;
+            });
+            // We're done with it so remove it
+            theMovieListWrapper.find('#pagination').remove();
+        } else {
+            // Remove the progress bar. We are done
+            theProgressBar.parent().remove();
+        }
+        // No one likes to go looking for the good ones :)
+        resolve(sortMovies(appendingMovieList));
+	});
+}
+
+const getMoviesInfo = (theMovieListWrappers, appendingMovieList, tabIndex) => {
+    return new Promise(function(resolve, reject) {
+        // For each list on the page with movies
+       	theMovieListWrappers.each(function( index ) {
+       		// Get the wrapped list of movies
+       		let theMovieListWrapper = $(this);
+       		// Help handle tabbing here
+       		let theChild = typeof tabIndex === 'undefined' || appendingMovieList == null ?
+       							(theMovieListWrapper.find('.movies-list').length > 1 ? ':first-child' : '') : ':nth-child('+tabIndex+')';
+       		// Get the full list of movies, show it incase we lost it
+       		let theMovieList = theMovieListWrapper.find('.movies-list'+theChild).show();
+       		// Set the list that needs to be applied to
+       		let theAppendingList = typeof appendingMovieList === 'undefined' || appendingMovieList == null ? theMovieList : appendingMovieList;
+       		// Add the progress bar if we dont have one
+       		if (!theAppendingList.find('.progress').length) {
+       			theAppendingList.prepend(
+       				'<div class="progress">'+
+       					'<div class="progress-bar progress-bar-striped active" role="progressbar" style="width:0%">'+
+       						'<span style="color:black;padding:10px 0;">Loading</span>'+
+       					'</div>'+
+       				'</div>'
+       			);
+       		}
+       		// A bit creepy but clone and yeah dig in
+       		theMovieList.find('.ml-item').each(function( index ) {
+       			// Get the elements we need
+       			let theMovieElem = $(this);
+       			let theMovieMaskElem = theMovieElem.find('.ml-mask');
+       			let thHeadingElem = theMovieElem.find('.mli-info');
+       			let theQualityElem = theMovieElem.find('.jtip-quality');
+       			let theTootipTopElem = theMovieElem.find('.jtip-top');
+       			// Get the title
+       			let theMovieTitle = theMovieMaskElem.attr('title');
+       			// Get the year
+       			let theYear = theTootipTopElem.find('.jt-info:nth-child(2)').text();
+       			// Get the IMDB rating
+                let theIMDbRating = parseInt(theTootipTopElem.find('.jt-imdb').text().replace('IMDb: ',''));
+       			// Get the Quality
+                let theQuality = theQualityElem.text().toUpperCase();
+                // If the Movie Quality list does not contain this quality
+                if (!allMovieQualitiesList.includes(theQuality)) {
+                    // Then add it
+                    allMovieQualitiesList.push(theQuality);
+                }
+       			// Get the full movie name
+       			let fullMovieName = theMovieTitle + ' (' + theYear + ')';
+       			// Check that its the right quality, has no duplicate and has not been marked as watched
+       			if (theAppendingList.find('.ml-item[title="'+theMovieTitle+'"]').length == 0
+       				&& theIMDbRating >= minIMBDRating
+       				&& movieQualitiesList.includes(theQuality.toUpperCase())
+       				&& !watchedMoviesList.includes(fullMovieName)) {
+       				// Okay we have a good quality movie
+       				// Now the fun starts
+       				// Get the info we want so badly from the tooltip top
+       				let theNewInfoElem = theTootipTopElem.clone()
+       													 .removeClass('jtip-top')
+       													 .addClass('mli-quality')
+       													 .css({'left' : 8, 'right' : 'auto'});
+       				// Now add the info right after the heading elem
+       				thHeadingElem.after(theNewInfoElem);
+       				// We need to record the IMDB rating + the year for sorting
+       				theMovieElem.attr('data-sorthandle', theYear+theIMDbRating);
+       				// Set the title
+       				theMovieElem.attr('title', fullMovieName);
+       				// Set the mask target and set the link to play mode
+       				theMovieMaskElem.attr('target','_blank')
+       								.attr('href', theMovieMaskElem.attr('href') + '?play=1');
+       				// Add the movie back in
+       				theAppendingList.append(theMovieElem);
+       				// Make sure the thumb is visible
+       				theMovieElem.find('.mli-thumb').attr('style','');
+       				// Add button to top for removing
+       				let theButton = $('<a>Remove</a>').addClass('jtip-quality markAsWatchedBtn text-center')
+       									 			  .css({'top':0,'width':'100%','height':18,'border-radius':'5px 5px 0 0','padding-top':'5px','z-index':9});
+       				theMovieElem.prepend(theButton);
+       				// Set additional styling
+       				theMovieMaskElem.css({'padding-top':'18px'});
+       				theMovieMaskElem.find('.mli-quality, .mli-eps').css({'top':'26px'});
+       				theButton.hover(function() {
+       				    $(this).css({'cursor':'pointer'});
+       				})
+       			} else {
+       				theMovieElem.remove();
+       			}
+       		});
+       		// Now some more helpfulness, no one likes to go clicking buttons for the next page!
+       		resolve(paginate(theMovieListWrapper, theAppendingList));
+       	});
+    });
+}
+
 var pagesToSearch;
-var watchedMovies;
+var minIMBDRating;
+var allMovieQualitiesList;
+var movieQualitiesList;
+var watchedMoviesList;
 
 const init = () => {
 	// Some virus protection
@@ -183,13 +202,26 @@ const init = () => {
 	chrome.storage.sync.get({
 		enabled: true,
     	pagesToSearch: 10,
+    	minIMBDRating: 6,
+    	allMovieQualities: ['HD','SD'],
+    	movieQualities: ['HD'],
     	watchedMovies: []
 	}, function(items) {
-		pagesToSearch = items.pagesToSearch;
-		watchedMovies = items.watchedMovies;
+		pagesToSearch = parseInt(items.pagesToSearch);
+		minIMBDRating = parseInt(items.minIMBDRating);
+		allMovieQualitiesList = items.allMovieQualities;
+		movieQualitiesList = items.movieQualities;
+		watchedMoviesList = items.watchedMovies;
 		if (items.enabled) {
 			// Get the info from movies
-			getMoviesInfo($('.movies-list-wrap'), null, null),
+			let getMoviesInfoPromise = getMoviesInfo($('.movies-list-wrap'), null, null);
+			// Update the All Movie Qualities List
+			getMoviesInfoPromise.then(function(value) {
+			    // Store all the movie qualities in the list
+                chrome.storage.sync.set({
+                    allMovieQualities: allMovieQualitiesList
+                }, function() {});
+            });
 			// Handle nav tab clicks
 			$(".nav-tabs").delegate('li', 'click', function () {
 				// Get the index
@@ -210,6 +242,7 @@ const init = () => {
 				}
 			});
 			// First remove all onclick events again for inserted elements
+			// @TODO check - still happening
 			removeOnClickEvents();
 			// Add button events
 			$(document).delegate('.markAsWatchedBtn', 'click', function(){
@@ -226,14 +259,14 @@ const init = () => {
 				chrome.storage.sync.get({
 					watchedMovies: []
 				}, function(items) {
-					watchedMovies = items.watchedMovies;
+					watchedMoviesList = items.watchedMovies;
 					// Add the movie to the list if not already there
 					if (!watchedMovies.includes(fullMovieName)) {
 						watchedMovies.push(fullMovieName);
 					}
 					// Store the movie name in the list
 					chrome.storage.sync.set({
-						watchedMovies: watchedMovies
+						watchedMovies: watchedMoviesList
 					}, function() {
 						// Remove it
 						theMovieElem.remove();
--- js/options.js
+++ js/options.js
@@ -1,17 +1,57 @@
+// Builds Quality list input
+const buildMovieQualityList = function(movieQualityList) {
+    chrome.storage.sync.get({
+        allMovieQualities: ['HD','SD']
+    }, function(items) {
+        // Clear container
+        $('.movieQualityListContainer').show();
+        $('.movieQualityList').html('');
+        // If we have qualities
+        if (typeof items.allMovieQualities !== 'undefined' && items.allMovieQualities.length
+            && typeof movieQualityList !== 'undefined' && movieQualityList.length) {
+            // Separate checked and unchecked markup
+            let theChecked = '';
+            let theUnchecked = '';
+            // Sort first
+            items.allMovieQualities.sort();
+            // For each movie
+            items.allMovieQualities.forEach(function(movieQuality) {
+                let checked = movieQualityList.includes(movieQuality);
+                let theMarkup = '<label>'+
+                                    '<button class="removeLabel" '+(checked ? 'disabled="disabled"' : '')+'>Remove</button>&nbsp;' +
+                                    '<input type="checkbox" name="movieQualityList[]" value="'+movieQuality+'" '+(checked ? 'checked="checked"' : '')+'/>'+
+                                    '&nbsp;' + movieQuality +
+                                '</label>'
+                if (checked) {
+                    theChecked += theMarkup;
+                } else {
+                    theUnchecked += theMarkup;
+                }
+            });
+            // Checked first then Unchecked
+            $('.movieQualityList').append(theChecked+theUnchecked);
+        } else {
+            $('.movieQualityListContainer').hide();
+        }
+    });
+}
+
 // Builds Watched Movie list input
-const buildWatchedMovies = function(watchedMovies) {
+const buildWatchedMoviesList = function(watchedMoviesList) {
     // Clear container
     $('.watchedMoviesListContainer').show();
     $('.watchedMoviesList').html('');
     // If we have movies
-    if (typeof watchedMovies !== 'undefined' && watchedMovies.length) {
+    if (typeof watchedMoviesList !== 'undefined' && watchedMoviesList.length) {
+        // Sort first
+        watchedMoviesList.sort();
         // For each movie
-        watchedMovies.forEach(function(watchedMovie) {
+        watchedMoviesList.forEach(function(watchedMovie) {
             $('.watchedMoviesList').append(
                 '<label>'+
-                    '<input type="checkbox" name="watchedMovies[]" value="'+watchedMovie+'" checked="checked"/>'+
+                    '<input type="checkbox" name="watchedMoviesList[]" value="'+watchedMovie+'" checked="checked"/>'+
                     '&nbsp;' + watchedMovie +
-                '</label><br/>'
+                '</label>'
             );
         });
     } else {
@@ -23,19 +63,32 @@ const buildWatchedMovies = function(watchedMovies) {
 const saveOptions = function() {
     let enabled = $('#enabled').prop('checked');
     let pagesToSearch = parseInt($('#pagesToSearch').val());
-    let watchedMovies = [];
-    $('input[name^="watchedMovies"]').each(function() {
+    let minIMBDRating = parseInt($('#minIMBDRating').val());
+    let allMovieQualityList = [];
+    let movieQualityList = [];
+    $('input[name^="movieQualityList"]').each(function() {
+        allMovieQualityList.push($(this).val());
         if ($(this).prop('checked')) {
-            watchedMovies.push($(this).val());
+            movieQualityList.push($(this).val());
+        }
+    });
+    let watchedMoviesList = [];
+    $('input[name^="watchedMoviesList"]').each(function() {
+        if ($(this).prop('checked')) {
+            watchedMoviesList.push($(this).val());
         }
     });
     chrome.storage.sync.set({
         enabled: enabled,
         pagesToSearch: pagesToSearch,
-        watchedMovies: watchedMovies
+        minIMBDRating: minIMBDRating,
+        allMovieQualities: allMovieQualityList,
+        movieQualities: movieQualityList,
+        watchedMovies: watchedMoviesList
     }, function() {
-        // Rebuild the watched movies
-        buildWatchedMovies(watchedMovies);
+        // Rebuild the lists
+        buildMovieQualityList(movieQualityList);
+        buildWatchedMoviesList(watchedMoviesList);
         // Update status to let user know options were saved.
         $('.status').text('Options saved.');
         setTimeout(function() {
@@ -44,18 +97,69 @@ const saveOptions = function() {
     });
 }
 
-// Restores with default or stored options
+// Filters lists of Quality and Movie Names
+const filterList = function() {
+    // Get the search input
+    let theSearchInput = $(this).val();
+    // If we have something to filter with
+    if (theSearchInput.length) {
+        // Run through elements and check
+        $(this).parent().find('div label').each(function() {
+            // Get the label and input
+            let theLabel = $(this);
+            let theInput = theLabel.find('input');
+            let theInputVal = theLabel.find('input').val();
+            // If the input value contains the filter criteria
+            if (theInputVal.toLowerCase().indexOf(theSearchInput.toLowerCase()) >= 0) {
+                // Then show the Label
+                theLabel.removeClass('hideLabel');
+            } else {
+                // Else hide
+                theLabel.addClass('hideLabel');
+            }
+        });
+    } else {
+        // Show all inputs
+        $(this).parent().find('div label').removeClass('hideLabel');
+    }
+}
+
+// Handle Label Clicks correctly
+const labelClick = function(e) {
+    // Prevent default behaviour
+    e.preventDefault();
+    // Now toggle any checkbox in the label
+    $(this).find('input[type="checkbox"]').prop('checked',!$(this).find('input[type="checkbox"]').prop('checked'));
+    // Based on the checkbox disable any buttons
+    $(this).find('button').prop('disabled',$(this).find('input[type="checkbox"]').prop('checked'));
+}
+
+// Remove Item from Quality List
+const removeLabel = function(e) {
+    // Remove the label
+    $(this).parent().remove();
+}
+
+// Init the lists
 const restoreOptions = function() {
+    // Restores with default or stored options
     chrome.storage.sync.get({
         enabled: true,
         pagesToSearch: 10,
+        minIMBDRating: 6,
+        movieQualities: ['HD'],
         watchedMovies: []
     }, function(items) {
         $('#enabled').prop('checked', items.enabled);
         $('#pagesToSearch').val(items.pagesToSearch);
-        buildWatchedMovies(items.watchedMovies);
+        $('#minIMBDRating').val(items.minIMBDRating);
+        buildMovieQualityList(items.movieQualities);
+        buildWatchedMoviesList(items.watchedMovies);
     });
 }
 
 document.addEventListener('DOMContentLoaded', restoreOptions);
 $(document).delegate('.save', 'click', saveOptions);
+$(document).delegate('#movieQualityListSearch, #watchedMoviesListSearch', 'keyup', filterList);
+$(document).delegate('label', 'click', labelClick);
+$(document).delegate('.removeLabel', 'click', removeLabel);
